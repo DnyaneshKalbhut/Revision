dea9e885db866429586bb99042ce5418df986ae0
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2025-01-02 09:56:09 +0530

    fdfd

---
+++ 01CRUDOperation/Models/Dac24Context.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace _01CRUDOperation.Models
+{
+    public partial class Dac24Context : DbContext
+    {
+        public Dac24Context()
+        {
+        }
+
+        public Dac24Context(DbContextOptions<Dac24Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Emp> Emps { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=Dac24;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Emp>(entity =>
+            {
+                entity.ToTable("Emp");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Address).HasMaxLength(200);
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ 01CRUDOperation/Models/Emp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01CRUDOperation.Models
+{
+    public partial class Emp
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+}
